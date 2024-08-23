4d6d0dd38b4cd53dbd9426a934314df1a1b21bdc
rigobertomadrid <135724248+rigobertomadrid@users.noreply.github.com>
2024-08-23 15:53:52 -0600

    Clase Funciones Parte Rigo

--- src/cmd/Funciones.java
+++ src/cmd/Funciones.java
@@ -96,5 +96,70 @@ public class Funciones {
         return "Error";
     }
     
+    public String getPath() {
+        try {
+            return dirActual.getCanonicalPath();
+        } catch (IOException e) {
+            return dirActual.getAbsolutePath();
+        }
+    }
     
+    public String mfile(String path) {
+        String msj = "";
+        File file = new File(path);
+        
+        if (file.exists()) {
+            msj = "Error: Este archivo ya existe";
+            return msj;
+        } else {
+            msj = "Archivo creado existosamente";
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                msj = "Error: no se pudo crear";
+            }
+            return msj;
+        }
+    }
+    
+    public static String leer(String path) {
+        File file = new File(path);
+        String msj = "";
+
+        if (file.exists()) {
+            if (file.isFile()) {
+                try {
+                    FileReader reader = new FileReader(file);
+                    String contenido = "";
+                    for (int i = reader.read(); i != -1; i = reader.read()) {
+                        contenido += (char) i;
+                    }
+                    return contenido;
+                } catch (IOException e) {
+                    msj = "Error: No se pudo leer";
+                    return msj;
+                }
+            } else {
+                msj = "Error: Debe seleccionar un archivo";
+                return msj;
+            }
+        } else {
+            msj = "Error: Archivo inexistente";
+            return msj;
+        }
+    }
+    
+    public static String listar(String path) {
+        String list = "";
+        File listFile = new File(path);
+        
+        if (listFile.isDirectory()) {
+            for (File file : listFile.listFiles()) {
+                list += "\n ->" + file.getName();
+            }
+            return list;
+        } else {
+            return "Error: Debe seleccionar un directorio";
+        }
+    }
 }
